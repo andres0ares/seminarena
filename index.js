037f8c33c773adda251a5082c412d2e432122106
Andre <andreiarley@gmail.com>
2020-06-09 13:22:07 -0300

    added calendariodeatividadedes.html, index.jx and soe functions to the index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+$(".fa-heart").click(function(){
+    $(this).toggleClass("heart-pressed");   
+})
+
+$(".fa-star").click(function(){
+    $(this).toggleClass("star-pressed");   
+})
+
+$(".fa-smile-o").click(function(){
+    $(this).toggleClass("star-pressed");   
+})
+
